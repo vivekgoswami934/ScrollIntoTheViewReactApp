a849861ec70b3ab6bd70bce87c8e3ca75590c094
vivekgoswami934 <vivek@rhombuz.io>
2023-11-12 17:06:52 +0530

    added the top bottom scrollintoview

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Box from "./Box";
+
+const getRandomColor = () => {
+  // Generate a random hex color code
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
 
 function App() {
+  const array = new Array(20).fill(0).map((_, index) => index + 1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        border: "1px solid black",
+        display: "flex",
+        gap: "1rem",
+        flexWrap: "wrap",
+        justifyContent: "center",  
+        alignItems: "center"
+      }}
+    >
+      {array.map((el, i) => {
+        return <Box key={i} color={getRandomColor()} i={i} />;
+      })}
     </div>
   );
 }
